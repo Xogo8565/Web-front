ae27bb1dd954fdf44f428cc68b887550cc3cb9aa
Xogo8565 <Xogo8565@gmail.com>
2022-10-04 16:13:09 +0900

    study : react effect

--- React/intro/src/App.js
+++ React/intro/src/App.js
@@ -1,11 +1,36 @@
-import Button from "./Button";
-import styles from "./Test.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("Run all the time");
+
+  useEffect(() => {
+    console.log("Run only once");
+  }, []); // excute only once when reloaded
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("search for", keyword);
+  }, [keyword]); // execute when keyword is changed
+  useEffect(() => {
+    console.log("Counter changed", counter);
+  }, [counter]); // excute when counter is changed
+  useEffect(() => {
+    console.log("Counter or Keyword changed");
+  }, [counter, keyword]); // excute when counter or keyword is changed
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
